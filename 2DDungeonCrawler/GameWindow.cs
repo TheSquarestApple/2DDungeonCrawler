8c52ba52953d8a04e456e55552f5762fdbbab5a1
thesquarestapple <thesquarestapple@gmail.com>
2016-04-13 20:29:21 +0100

    test 1

---
+++ 2DDungeonCrawler/GameWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DDungeonCrawler
+{
+    public partial class GameWindow : Form
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
